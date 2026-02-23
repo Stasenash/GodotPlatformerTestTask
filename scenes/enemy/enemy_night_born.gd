90f6e63e36cd74784b843b6b1e1ec4855623e4ef
Stasenash <staslisenash@gmail.com>
2026-02-23 20:01:26 +0500

    Added patrol state for enemy

--- scenes/enemy/enemy_night_born.gd
+++ scenes/enemy/enemy_night_born.gd
@@ -3,16 +3,19 @@ extends CharacterBody2D
 const SPEED: float = 100.0
 
 enum State {
-	IDLE,
+	PATROL,
 	CHASE,
 	DEAD
 }
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var player: CharacterBody2D = get_parent().get_node("Player")
+@onready var left_ray: RayCast2D = $LeftEdgeRay
+@onready var right_ray: RayCast2D = $RightEdgeRay
 
-var state: State = State.IDLE
 
+var state: State = State.PATROL
+var direction: int = 1   # 1 = вправо, -1 = влево
 
 func _physics_process(delta: float) -> void:
 	if state == State.DEAD:
@@ -34,33 +37,58 @@ func _apply_gravity(delta: float) -> void:
 func _update_state() -> void:
 	if state == State.DEAD:
 		return
-	# IDLE и CHASE переключаются через detector
+	# PATROL и CHASE переключаются через detector
 	pass
-
 
 func _change_state(new_state: State) -> void:
 	state = new_state
 
 
 func _update_movement() -> void:
-	if state != State.CHASE:
-		velocity.x = 0
-		return
+	match state:
+		State.PATROL:
+			_patrol()
+		State.CHASE:
+			_chase()
+		State.DEAD:
+			velocity = Vector2.ZERO
+	
 
-	var direction := (player.global_position - global_position).normalized()
-	velocity.x = direction.x * SPEED
+func _patrol() -> void:
+	velocity.x = direction * SPEED
 
-	if direction.x != 0:
-		anim.flip_h = direction.x < 0
+	# Проверка края платформы
+	if direction == 1 and not right_ray.is_colliding():
+		_flip()
+	elif direction == -1 and not left_ray.is_colliding():
+		_flip()
+
+	anim.flip_h = direction < 0
 
 
+func _chase() -> void:
+	var to_player := player.global_position - global_position
+	var dir_x: int = 0
+
+	if to_player.x > 0:
+		dir_x = 1
+	elif to_player.x < 0:
+		dir_x = -1
+
+	velocity.x = dir_x * SPEED
+
+	if dir_x != 0:
+		anim.flip_h = dir_x < 0
+
+func _flip() -> void:
+	direction *= -1
 
 func _update_animation() -> void:
 	match state:
-		State.IDLE:
-			_play_animation("Idle")
-		State.CHASE:
+		State.PATROL:
 			_play_animation("Walk")
+		State.CHASE:
+			_play_animation("WalkFaster")
 		State.DEAD:
 			_play_animation("Death")
 
@@ -78,7 +106,7 @@ func _on_detector_body_entered(body: Node2D) -> void:
 
 func _on_detector_body_exited(body: Node2D) -> void:
 	if body == player:
-		_change_state(State.IDLE)
+		_change_state(State.PATROL)
 
 
 func die() -> void:
